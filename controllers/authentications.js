dc1c1c339df0ad7a92456d1a8d81c43a67fbf821
Benjamin Chang <ben.chang@hotmail.co.uk>
2017-08-13 15:41:21 +0100

    added tokens

--- controllers/authentications.js
+++ controllers/authentications.js
@@ -1,11 +1,15 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
 
 function register(req, res) {
   User.create(req.body)
     .then(data => {
+      const token = jwt.sign({ id: data.id }, config.secret, { expiresIn: 86400 });
       return res.status(201).json({
         message: `Welcome, ${data.name}`,
-        data
+        data,
+        token
       });
     })
     .catch(err => {
@@ -20,9 +24,11 @@ function login(req, res) {
       if (!data || !data.validatePassword(req.body.password)){
         return res.status(401).json({ message: 'Unrecognised credentials.' });
       }
+      const token = jwt.sign({ id: data.id }, config.secret, { expiresIn: 86400 });
       return res.status(201).json({
         message: `Welcome back, ${data.name}`,
-        data
+        data,
+        token
       });
     })
     .catch(err => {
