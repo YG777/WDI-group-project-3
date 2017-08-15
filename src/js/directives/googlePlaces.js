351a688e63aed34a0a87c633e5dab3004841c599
mikeBelither <mikebelither@hotmail.co.uk>
2017-08-15 11:57:06 +0100

    basic google-places functionality added

--- src/js/directives/googlePlaces.js
+++ src/js/directives/googlePlaces.js
@@ -2,57 +2,39 @@ angular
 .module('group-proj')
 .directive('googlePlaces', googlePlaces);
 
-
 googlePlaces.$inject = ['$window'];
-function googlePlaces($window){
+function googlePlaces($window) {
   const directive = {
     restrict: 'E',
     replace: true,
-    template: '<div class="google-places">Google Places</div>',
+    template: '<input type="text" id="google-places" />',
     scope: {
       center: '='
     },
     link($scope, element) {
-      new $window.google.maps.places.PlacesService(element[0]);
       console.log(element[0]);
-      const place = new $window.google.places.Map(element[0], {
-        center: $scope.center
-      });
 
-      new $window.google.places.Marker({
-        position: $scope.center,
-        place: place,
-        animation: $window.google.places.Animation.DROP
-      });
+      var placeSearch, autocomplete;
+      var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+      };
+
+      autocomplete = new $window.google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */element[0],
+            {types: ['establishment']});
+
+        // autocomplete.addListener('place_changed', fillInAddress);
     }
-  };
 
+
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+
+  };
+  return directive;
 }
-//
-// [ '$parse', '$compile', '$timeout', '$document', 'googlePlacesApi',
-// function ($parse, $compile, $timeout, $document, google) {
-//
-//   return {
-//     restrict: 'A',
-//     require: '^ngModel',
-//     scope: {
-//       model: '=ngModel',
-//       options: '=?',
-//       forceSelection: '=?',
-//       customPlaces: '=?'
-//     },
-//     controller: ['$scope', function ($scope) {}],
-//     link: function ($scope, element, attrs, controller) {
-//       var keymap = {
-//         tab: 9,
-//         enter: 13,
-//         esc: 27,
-//         up: 38,
-//         down: 40
-//       },
-//       hotkeys = [keymap.tab, keymap.enter, keymap.esc, keymap.up, keymap.down],
-//       autocompleteService = new google.maps.places.AutocompleteService(),
-//       placesService = new google.maps.places.PlacesService(element[0]);
-//
-//     }
-//   }
