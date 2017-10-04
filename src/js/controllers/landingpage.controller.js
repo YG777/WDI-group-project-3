70c67f8b102722cf0b2e18b1c6ef224cd177e6aa
Benjamin Chang <ben.chang@hotmail.co.uk>
2017-10-03 21:45:49 -0700

    debugging heroku deployment

--- src/js/controllers/landingpage.controller.js
+++ src/js/controllers/landingpage.controller.js
@@ -7,19 +7,19 @@ function LandingPageCtrl($interval, $rootScope) {
   vm.landingPage = true;
   vm.meals = ['Breakfast', 'Lunch', 'Dinner', 'the Pub', 'Bar crawls'];
   $rootScope.$broadcast('landingPage');
-  vm.cycleMeals = cycleMeals;
+  // vm.cycleMeals = cycleMeals;
 
   vm.meal = vm.meals[0];
 
-  vm.cycleMeals();
-  function cycleMeals() {
-    let index = 0;
-    $interval(() => {
-      if (index === 4) index = 0;
-      vm.meal = vm.meals[index];
-      index++;
-    }, 2000);
-  }
+  // vm.cycleMeals();
+  // function cycleMeals() {
+  //   let index = 0;
+  //   $interval(() => {
+  //     if (index === 4) index = 0;
+  //     vm.meal = vm.meals[index];
+  //     index++;
+  //   }, 2000);
+  // }
 
 
 }
